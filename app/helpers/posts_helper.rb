80839a2aadae7d3b766a3579954d3b167795de2b
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-06 02:22:48 +0000

    automatically backed up by learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def last_updated(post)
-  post.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
-end
+    post.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  end
 
 end
